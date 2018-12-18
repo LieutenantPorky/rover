dc3e032dee1173e9ba489a995bacf7d27afbce54
LieutenantPorky <jacopo@siniscalco.eu>
2018-12-18 14:18:26 +0000

    created file hierarchy and initiated first interface controller

---
+++ interface/GUI_interface_v1.py
@@ -0,0 +1,12 @@
+#! python
+
+'''
+V 1.0 GUI serial interface for robot - allows to define
+bytecodes to be sent, logs data
+'''
+
+import json
+import tkinter
+import keyboard
+import serial
+import io
